ed41e06f1b2c12fccf6b264d32bca290140e3dce
Chahat-23 <chahatshrivastava230620@gmail.com>
2025-01-06 22:34:14 +0530

    'Header-completed-for-mobile-view'

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Header.css";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/logo.svg";
+import {HiMenu} from "react-icons/hi";
+import { IoMdClose } from "react-icons/io";
 
 export default function Header() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  useEffect(()=> {
+    if(isMenuOpen)
+      document.body.classList.add('menu-open');
+    else
+      document.body.classList.remove("menu-open");
+
+    return() => {
+      document.body.classList.remove("menu-open");
+    };
+  }, [isMenuOpen]);
+
   return (
     <div className="header-wrap">
       <div className="header">
@@ -18,7 +38,26 @@ export default function Header() {
             <li>Contact Us</li>
           </ul>
         </nav>
-        <button className="cta-button">Let's Talk</button>
+        <button className="cta-button desktop-cta">Let's Talk</button>
+
+        <div className="hamburger-icon" onClick={toggleMenu}>
+          {isMenuOpen ? (
+            <IoMdClose size={28} color="white"/>
+          ) : (
+            <HiMenu size={28} color="white"/>
+          )}
+        </div>
+
+        <div className={`mobile-menu ${isMenuOpen ? 'active' : ''}`}>
+          <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Our Process</li>
+            <li>Clients</li>
+            <li>Contact Us</li>
+          </ul>
+          <button className="cta-button mobile-cta">Let's Talk</button>
+        </div>
       </div>
     </div>
   );
