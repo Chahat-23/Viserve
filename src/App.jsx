561db327b4ffe1ab5478a3ddfbadee7950a05b68
Chahat-23 <chahatshrivastava230620@gmail.com>
2025-01-08 01:29:34 +0530

    'homepage-done-desktop-and-mobile'

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
+import HomePage from './Components/HomePage/HomePage'
 
 function App() {
 
   return (
     <>
       <Header/>
+      <HomePage/>
     </>
   )
 }
---
+++ src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./HomePage.css"
+import Marquee from "react-fast-marquee";
+
+export default function HomePage() {
+  return (
+    <div className="home">
+      <div className="homepage">
+        <div className="content-wrapper">
+          <div className="homepage-title">
+            Your Go-To Marketing Wizards for a Successful Digital Journey
+          </div>
+          <div className="homepage-desc">
+            Engage, Convert, and Grow with Our Expert Digital Solutions - We're
+            the Digital Partners You Need for Growth
+          </div>
+          <button className="homepage-cta">Schedule a Call -></button>
+        </div>
+      </div>
+
+      <div className="background-strip"></div>
+
+      <div className="marquee-container">
+        <Marquee gradient={false} speed={100}>
+          <div className="marquee-item">DIGITAL MARKETING</div>
+          <div className="marquee-item">*</div>
+          <div className="marquee-item">WEB DEVELOPMENT</div>
+          <div className="marquee-item">*</div>
+          <div className="marquee-item">SEO</div>
+          <div className="marquee-item">*</div>
+          <div className="marquee-item">CONTENT STRATEGY</div>
+          <div className="marquee-item">*</div>
+          <div className="marquee-item">SOCIAL MEDIA</div>
+          <div className="marquee-item">*</div>
+          <div className="marquee-item">BRAND GROWTH</div>
+          <div className="marquee-item">*</div>
+        </Marquee>
+      </div>
+    </div>
+  );
+}
